953a8c4bf2dffa501fa08d42fc81917204d4aeaa
Volodymyr Viter <mad.tvido@gmail.com>
2021-04-14 21:29:53 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-import './styles.css';
-import './js/fetchCountries';
+// import './css/common.css';
+import './js/fetchCountries.js';
+import countryItem from './templates/items.hbs';
+import countryList from './templates/list.hbs'
